67574685b7b16be2cd397e4a84d1c3150726bca1
hugosenari <hugosenari@gmail.com>
2022-03-12 02:35:25 -0300

    chore: re index datetime2human

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Human friendly DateTime string representations, seconds to millenniums.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."datetime2human-master".url = "path:./master";
+  inputs."datetime2human-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."datetime2human-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Human friendly DateTime string representations, seconds to millenniums.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-datetime2human-master.flake = false;
+  inputs.src-datetime2human-master.owner = "juancarlospaco";
+  inputs.src-datetime2human-master.ref   = "refs/heads/master";
+  inputs.src-datetime2human-master.repo  = "nim-datetime2human";
+  inputs.src-datetime2human-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-datetime2human-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-datetime2human-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
